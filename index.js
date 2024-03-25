1608d6b52f1a0b200d2bd36c36766e4abdaef233
Samwel <samwelnyandoro01@gmail.com>
2024-03-25 09:15:14 +0300

    created a successful db connection

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const mysql = require("mysql2");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+const dbConn = mysql.createConnection({
+  host: process.env.DB_HOST || "",
+  user: process.env.DB_USER_NAME || "",
+  password: process.env.DB_USER_PASSWORD || "",
+  database: process.env.DB_NAME || "",
+  port: process.env.DB_PORT || "",
+});
+
+dbConn.connect(function (err) {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("Database was connected");
+  require("./routes")({ app, dbConn });
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+});
